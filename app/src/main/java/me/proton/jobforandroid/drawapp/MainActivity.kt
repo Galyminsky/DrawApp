3edc4302afbd7aaf9d7fbd4ec221ddf61d8fa053
Galyaminsky_Note <jobforandroid@proton.me>
2024-05-23 20:03:11 +0500

    #2

--- app/src/main/java/me/proton/jobforandroid/drawapp/MainActivity.kt
+++ app/src/main/java/me/proton/jobforandroid/drawapp/MainActivity.kt
@@ -4,7 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInput
 import me.proton.jobforandroid.drawapp.ui.theme.DrawAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,4 +34,34 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DrawCanvas() {
 
+    val tempPath = Path()
+    val path = remember {
+        mutableStateOf(Path())
+    }
+
+    Canvas(modifier = Modifier.fillMaxSize()
+        .pointerInput(true) {
+            detectDragGestures { change, dragAmount ->
+                tempPath.moveTo(
+                    change.position.x - dragAmount.x,
+                    change.position.y - dragAmount.y
+                )
+
+                tempPath.lineTo(
+                    change.position.x,
+                    change.position.y
+                )
+
+                path.value = Path().apply {
+                    addPath(tempPath)
+                }
+            }
+        }
+    ) {
+        drawPath(
+            path.value,
+            color = Color.Red,
+            style = Stroke(5f)
+        )
+    }
 }
