0d0c7f1b2c431e42a89f21315c59c49038da7fd0
Galyaminsky_Note <jobforandroid@proton.me>
2024-05-23 12:08:22 +0500

    Start

---
+++ app/src/main/java/me/proton/jobforandroid/drawapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package me.proton.jobforandroid.drawapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import me.proton.jobforandroid.drawapp.ui.theme.DrawAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DrawAppTheme {
+
+            }
+        }
+    }
+}
+
+
+@Composable
+fun DrawCanvas() {
+
+}
