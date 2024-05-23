f757ae36b6cb6ebf7c144d4b0ab232a766b8b3bb
Galyaminsky_Note <jobforandroid@proton.me>
2024-05-23 20:12:35 +0500

    #3

---
+++ app/src/main/java/me/proton/jobforandroid/drawapp/ui/theme/BottomPanel.kt
@@ -0,0 +1,27 @@
+package me.proton.jobforandroid.drawapp.ui.theme
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BottomPanel() {
+
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+
+    ) {
+
+    }
+}
+
+@Composable
+fun ColorList() {
+    val colors = listOf(
+
+    )
+}
